1ef67ad20b8f2b1e0700a1514c872eb75ec8bb3d
ManonMaMa <infini030303@gmail.com>
2025-11-12 10:48:45 +0100

    correction erreur commit precedent

--- projet_liens.py
+++ projet_liens.py
@@ -36,7 +36,5 @@ def video():
 
 # Lancement du serveur : mode debug et hot reload actif.
 if __name__ == '__main__':
-    with app.app_context():
-        database.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
 
